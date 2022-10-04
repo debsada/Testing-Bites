77db038235d01858078bc87f245ecab679758086
deborah <deborahnwankwo1998@gmail.com>
2022-10-04 12:02:16 +0100

    present & test

---
+++ spec/present_spec.rb
@@ -0,0 +1,20 @@
+require "present"
+
+RSpec.describe Present do
+    it "wraps and unwraps a value" do 
+        present = Present.new
+        present.wrap(6)
+        expect(present.unwrap). to eq 6
+    end 
+
+    it "fails if we unwrap without wrapping first" do
+        present = Present.new
+        expect {present.unwrap}.to raise_error "No contents have been wrapped"
+    end
+
+    it "fails if we have already wrapped something" do 
+        present = Present.new
+        present.wrap(6)
+        expect {present.wrap(6)}.to raise_error "A contents has already been wrapped."
+    end
+end
